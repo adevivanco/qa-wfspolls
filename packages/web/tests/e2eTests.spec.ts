77461e0961bfea8f5b65e7af1d7b5bf198651ae0
Andres de Vivanco <andy.dv.cm@gmail.com>
2024-01-16 11:33:01 -0500

    first commit to skeleton

--- packages/web/tests/e2eTests.spec.ts
+++ packages/web/tests/e2eTests.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 import { LoginSteps } from "../steps/loginSteps";
-import { RegisterSteps } from "../steps/registerSteps";
-import { PlayerPollsSteps } from "../steps/playerPollsSteps";
 import  webEnvironment from "../environment/webEnvironment";
 test.beforeEach(async ({ page }) => {
   await page.goto(`${webEnvironment.BASE_URL}/login`);
@@ -15,25 +13,3 @@ test('validate main elements', async ({ page }) => {
   await loginSteps.clickOnRegisterLink();
 });
 
-test('validate main elements and navigate to registration page', async ({ page }) => {
-  const loginSteps = new LoginSteps(page);
-  const registerSteps:RegisterSteps = new RegisterSteps(page);
-
-  await loginSteps.waitForUrl(webEnvironment.BASE_URL);
-  await loginSteps.validateMainElements();
-  await loginSteps.clickOnRegisterLink();
-  await registerSteps.waitForUrl(webEnvironment.BASE_URL);
-});
-
-test('validate main elements and login using valid credentials', async ({ page }) => {
-  const loginSteps = new LoginSteps(page);
-  const playerPollsSteps  = new PlayerPollsSteps(page);
-
-  await loginSteps.waitForUrl(webEnvironment.BASE_URL);
-  await loginSteps.validateMainElements();
-  await loginSteps.enterUsernameAndPassword(webEnvironment.USERNAME, webEnvironment.USER_PASSWORD);
-  await loginSteps.clickLogInButton();
-
-  await playerPollsSteps.waitForUrl(webEnvironment.BASE_URL);
-  await playerPollsSteps.validateMainElements();
-});
